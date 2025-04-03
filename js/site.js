386ce2cbc87e0ea709d0a26abf3317a1dc11abf6
Guillermo Vitola <vitola.guillermo@gmail.com>
2025-04-03 07:21:41 -0500

    Updates

---
+++ js/site.js
@@ -0,0 +1,149 @@
+const url = 'https://puerto54.com:44381/api/todoitems';
+
+
+let todos = [];
+
+async function getItems() {
+  const options = {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    }
+  };
+  
+  await fetch(url, options)
+      .then(response => response.json())
+      .then(data => _displayItems(data))
+      .catch(error => console.error('Unable to get items.', error))
+}
+
+function addItem() {
+  const addNameTextbox = document.getElementById('add-name');
+
+  const item = {
+    isComplete: false,
+    name: addNameTextbox.value.trim()
+  };
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(item)
+  };
+
+  fetch(url, options)
+    .then(response => response.json())
+    .then(() => {
+      getItems();
+      addNameTextbox.value = '';
+    })
+    .catch(error => console.error('Unable to add item.', error));
+}
+
+function deleteItem(id) {
+  const options = {
+      method: 'DELETE'
+  }
+
+  fetch(`${url}/${id}`, options)
+    .then(() => getItems())
+    .catch(error => console.error('Unable to delete item.', error));
+}
+
+function updateItem() {
+  const itemId = document.getElementById('edit-id').value;
+  const item = {
+    id: parseInt(itemId, 10),
+    isComplete: document.getElementById('edit-isComplete').checked,
+    name: document.getElementById('edit-name').value.trim()
+  };
+
+  const options = {
+    method: 'PUT',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(item)
+  };
+
+  fetch(`${url}/${itemId}`, options)
+    .then(() => getItems())
+    .catch(error => console.error('Unable to update item.', error));
+
+  closeEditForm();
+
+  return false;
+}
+
+function displayEditForm(id) {
+  const item = todos.find(item => item.id === id);
+  
+  document.getElementById('edit-name').value = item.name;
+  document.getElementById('edit-id').value = item.id;
+  document.getElementById('edit-isComplete').checked = item.isComplete;
+  document.getElementById('editForm').style.display = 'block';
+}
+
+function closeEditForm() {
+  document.getElementById('editForm').style.display = 'none';
+}
+
+function _displayCount(itemCount) {
+  const name = (itemCount === 1) ? 'to-do' : 'to-dos';
+
+  document.getElementById('counter').innerText = `${itemCount} ${name}`;
+}
+
+function _displayItems(data) {
+  const tBody = document.getElementById('todos');
+  tBody.innerHTML = '';
+
+  _displayCount(data.length);
+
+  if (data.length === 0) {
+    document.getElementById('listTable').style.display = 'none';
+    document.getElementById('counter').style.display = 'none';
+  } else {
+    document.getElementById('listTable').style.display = 'table';
+    document.getElementById('counter').style.display = 'block';
+  }
+
+  const button = document.createElement('button');
+
+  data.forEach(item => {
+    let isCompleteCheckbox = document.createElement('input');
+    isCompleteCheckbox.type = 'checkbox';
+    isCompleteCheckbox.disabled = true;
+    isCompleteCheckbox.checked = item.isComplete;
+
+    let editButton = button.cloneNode(false);
+    editButton.innerText = 'Edit';
+    editButton.setAttribute('onclick', `displayEditForm(${item.id})`);
+
+    let deleteButton = button.cloneNode(false);
+    deleteButton.innerText = 'Delete';
+    deleteButton.setAttribute('onclick', `deleteItem(${item.id})`);
+
+    let tr = tBody.insertRow();
+    
+    let td1 = tr.insertCell(0);
+    td1.appendChild(isCompleteCheckbox);
+
+    let td2 = tr.insertCell(1);
+    let textNode = document.createTextNode(item.name);
+    td2.appendChild(textNode);
+
+    let td3 = tr.insertCell(2);
+    td3.appendChild(editButton);
+
+    let td4 = tr.insertCell(3);
+    td4.appendChild(deleteButton);
+  });
+
+  todos = data;
+}
